1dc18c778c0aa812f14447c47de6366e25731693
viniciusestevamsilva <viniciusestevamsilvajf@gmail.com>
2025-10-11 14:17:57 -0300

    UPD: Renomei os arquivos da atividade e terminei a E

---
+++ Dart/Atividades/Atividade1_DesenvolvimentoMovel/E-Dividindo4formatada.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+void main() {
+
+  double? num1;
+  double? num2;
+  
+  print('-' * 50);
+
+  stdout.write('Digite o primeiro número: ');
+  String? valor1 = stdin.readLineSync();
+
+  if (valor1 != null && valor1.isNotEmpty) {
+    num1 = double.tryParse(valor1);
+  }
+
+  stdout.write('Digite o segundo número: ');
+  String? valor2 = stdin.readLineSync();
+
+  if (valor2 != null && valor2.isNotEmpty) {
+    num2 = double.tryParse(valor2);
+  }
+
+  if (num1 == null || num2 == null) {
+    print('Erro: Entrada inválida.');
+    return;
+  }
+
+  if (num2 == 0) {
+    print('-' * 50);
+    print('ERRO: Dividir por zero não é permitida.');
+    print('-' * 50);
+    return;
+  }
+
+  double resultado = num1 / num2;
+
+  print('-' * 40);
+  print('Resultado da divisão: ${resultado.toStringAsFixed(4)}');
+  print('-' * 40);
+}
